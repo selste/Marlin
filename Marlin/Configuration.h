dc2692c681e5a8f7467a157388e77062f401478e
Steffen Elste <steffen.elste@gmail.com>
2019-11-17 11:33:06 +0100

    Print counter

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1573,7 +1573,7 @@
  *
  * View the current statistics with M78.
  */
-//#define PRINTCOUNTER
+#define PRINTCOUNTER
 
 //=============================================================================
 //============================= LCD and SD support ============================
